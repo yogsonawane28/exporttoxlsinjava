595e02b16d908e085efbd8009fd86b99f481ab94
Yogesh Sonawane <32389776+yogsonawane28@users.noreply.github.com>
2017-09-29 14:58:51 +0200

    Update ExportController.java

--- ExportController.java
+++ ExportController.java
@@ -2,31 +2,23 @@
 	public void getExportFile(Map<String, Object> map,HttpServletRequest request,HttpServletResponse response) {
 		InputVO inputVO=null;
     try {
-			if(null!=request.getParameter(RequestParameter.getString("json"))){
-				  ObjectMapper mapper = auditMapperFactory.getInstance("mapper-instance");
-				  inputVO =  mapper.readValue(request.getParameter(RequestParameter.getString("json")), InputVO.class);
-				}
-			    response.setContentType(CONTROLLER_CONSTANTS.APPLICATION_VND_MS_EXCEL);
-			    String fileName=CONSTANTS.EMPTY;
-			    response.setHeader("Content-Disposition",  Util.stringConcatenate("attachment; filename=",inputVO.fileName," "));
-		      HSSFWorkbook hwb = new HSSFWorkbook();
-		      HSSFSheet sheet=service.getEXCELData(inputs); // this will get data from database and will populate it to excel object 
+	 if(null!=request.getParameter(RequestParameter.getString("json"))){
+		ObjectMapper mapper = auditMapperFactory.getInstance("mapper-instance");
+		inputVO =  mapper.readValue(request.getParameter(RequestParameter.getString("json")), InputVO.class);
+	 }
+		response.setContentType(CONTROLLER_CONSTANTS.APPLICATION_VND_MS_EXCEL);
+		response.setHeader("Content-Disposition",  Util.stringConcatenate("attachment; filename=",inputVO.fileName," "));
+		HSSFWorkbook hwb = new HSSFWorkbook();
+		HSSFSheet sheet=service.getEXCELData(inputs); // this will get data from database and will populate it to excel object 
 	        ServletOutputStream outputStream = response.getOutputStream();
 	        sheet.getWorkbook().write(outputStream);
 	        outputStream.flush();
-          
-		} catch (JsonGenerationException e) {
-			LOGGER.error(e.getMessage());		
-
-		} catch (JsonMappingException e) {
-			LOGGER.error(e.getMessage());	
-	
-		} catch (IOException e) {
-			LOGGER.error(e.getMessage());	
-	
-		}
-		catch (NTPRuntimeException ntpRuntimeException) {
-      LOGGER.error(e.getMessage());	
-		}
-		
+      	} catch (JsonGenerationException e) {
+		LOGGER.error(e.getMessage());		
+	} catch (JsonMappingException e) {
+		LOGGER.error(e.getMessage());	
+	} catch (IOException e) {
+		LOGGER.error(e.getMessage());	
 	}
+		
+}
